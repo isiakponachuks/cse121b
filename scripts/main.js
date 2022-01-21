8f5ca06c1697a640883179ffa94883e75e04c3e3
isiakponachuks <isi21001@byui.edu>
2022-01-21 15:15:56 +0100

    comment

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,22 @@ function copyInput() {
 }
 buttonElement.addEventListener("click", copyInput);
 
+// modify the copyInput callback to receive the event object
+function copyInput(event) {
+    // take a look at the event!
+    console.log(event);
+    const inputElement = document.getElementById("inputBox");
+    const outputElement = document.getElementById("output");
+    outputElement.innerHTML = inputElement.value;
+  }
+
+  const log = document.querySelector("#log");
+
+document.addEventListener("keydown", logKey);
+
+function logKey(e) {
+  // how do we know which key was pressed?
+  console.log(e);
+  // checkout e.code, e.key, and e.keyCode
+  // what is the difference?
+
