16ec211b60b28323361dd899b91bddceb553a7f2
isiakponachuks <isi21001@byui.edu>
2022-04-14 22:04:17 +0100

    comit

---
+++ lesson5/styles/debuging.js
@@ -0,0 +1,12 @@
+function circleArea (radius)
+
+const PI = 3.14;
+let area = 0;
+
+function circleArea(radius) {
+    const area = radius * PI;
+  }
+  area = circleArea(3);
+
+
+console.log(area);
